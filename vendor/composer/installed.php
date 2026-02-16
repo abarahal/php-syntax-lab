561bbdf5584e4f916bf6b274d335156d59cea2d3
abarahal <a.barahal@void.fr>
2026-02-16 16:52:53 +0000

    update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3ddd455d2288420b64c55cf5c6b9a06f2cba4237',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3ddd455d2288420b64c55cf5c6b9a06f2cba4237',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.1.39',
+            'version' => '2.1.39.0',
+            'reference' => 'c6f73a2af4cbcd99c931d0fb8f08548cc0fa8224',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
